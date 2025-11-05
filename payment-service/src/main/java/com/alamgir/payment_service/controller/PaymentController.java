1a4a76991c448801925f8f8aed27d476ada5ec48
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-11-05 22:15:04 +0600

    updated

--- payment-service/src/main/java/com/alamgir/payment_service/controller/PaymentController.java
+++ payment-service/src/main/java/com/alamgir/payment_service/controller/PaymentController.java
@@ -2,14 +2,18 @@ package com.alamgir.payment_service.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alamgir.payment_service.entity.Payment;
 import com.alamgir.payment_service.request.PaymentRequest;
 import com.alamgir.payment_service.service.PaymentService;
 
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class PaymentController {
@@ -20,6 +24,12 @@ public class PaymentController {
     @PostMapping("/create")
     public String createPayment(@Valid @RequestBody PaymentRequest paymentRequest) {
         return paymentService.createPayment(paymentRequest);
+
+    }
+
+    @GetMapping("check")
+    public List<Payment> getAllPaymentDetails() {
+        return paymentService.getAllPaymentDetails();
 
     }
 
--- payment-service/src/main/java/com/alamgir/payment_service/service/PaymentService.java
+++ payment-service/src/main/java/com/alamgir/payment_service/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.alamgir.payment_service.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,12 @@ public class PaymentService {
 
         }
         return "Payment Error";
+
+    }
+
+    public List<Payment> getAllPaymentDetails() {
+        List<Payment> payments = paymentRepository.findAll();
+        return payments;
 
     }
 
